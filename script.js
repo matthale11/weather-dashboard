5c32ac896846a29f57c73df5e8276f440639db40
Matt Hale <mhale@factharbor.com>
2021-03-30 10:08:32 -0400

    added function and event listener for submission

--- script.js
+++ script.js
@@ -1,90 +1,102 @@
 var cityDateEl = document.querySelector('#city-date');
 var currentEl = document.querySelector('#current');
 var forecastEl = document.querySelector('#forecast');
+var submitBtn = document.getElementById('#submit-btn');
 
 // API key: dc2f3090dc723dd5dfe242a2abd2e604
-// Fetch information using the Current Weather endpoint
-var cityName = 'Atlanta';
-var currentDate = moment().format('MM/DD/YYYY');
-var currentUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&units=imperial&appid=dc2f3090dc723dd5dfe242a2abd2e604';
 
-// Display searched city and current date
-cityDateEl.textContent = cityName + ' (' + currentDate + ')';
+// TODO: Save city name to local storage
 
 // Fetch data from OpenWeather API and modify DOM
-fetch (currentUrl).then(function(response) {
-    if (response.ok) {
-        response.json().then(function(data) {
+function getWeather (cityName) {
 
-            // Create weather icon and append
-            var iconEl = document.createElement('img');
-            iconEl.setAttribute('src', 'https://openweathermap.org/img/wn/' + data.weather[0].icon + '.png');
-            cityDateEl.appendChild(iconEl);
+    // Fetch information using the Current Weather endpoint
+    var currentDate = moment().format('MM/DD/YYYY');
+    var currentUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&units=imperial&appid=dc2f3090dc723dd5dfe242a2abd2e604';
 
-            // Create temperature element
-            var tempEl = document.createElement('p');
-            tempEl.textContent = 'Temperature: ' + data.main.temp + ' °F';
-            currentEl.appendChild(tempEl);
+    fetch (currentUrl).then(function(response) {
+        if (response.ok) {
+            response.json().then(function(data) {
 
-            // Create humidity element and append
-            var humidityEl = document.createElement('p');
-            humidityEl.textContent = 'Humidity: ' + data.main.humidity + '%';
-            currentEl.appendChild(humidityEl);
+                // Display searched city and current date
+                cityDateEl.textContent = data.name + ' (' + currentDate + ')';
+                // Create weather icon and append
+                var iconEl = document.createElement('img');
+                iconEl.setAttribute('src', 'https://openweathermap.org/img/wn/' + data.weather[0].icon + '.png');
+                cityDateEl.appendChild(iconEl);
 
-            // Create wind speed element and append
-            var windSpeedEl = document.createElement('p');
-            windSpeedEl.textContent = 'Wind Speed: ' + data.wind.speed + ' MPH';
-            currentEl.appendChild(windSpeedEl);
+                // Create temperature element
+                var tempEl = document.createElement('p');
+                tempEl.textContent = 'Temperature: ' + data.main.temp + ' °F';
+                currentEl.appendChild(tempEl);
 
-            // Get lat/lon coordinates and use this to fetch UV index
-            var uviUrl = 'https://api.openweathermap.org/data/2.5/uvi?lat=' + data.coord.lat + '&lon=' + data.coord.lon + '&appid=dc2f3090dc723dd5dfe242a2abd2e604';
-            // Run fetch for this UVI URL and append 
-            fetch (uviUrl).then(function(response) {
-                if (response.ok) {
-                    response.json().then(function(data) {
-                    var uviEl = document.createElement('p');
-                    uviEl.textContent = 'UV Index: ' + data.value;
-                    currentEl.appendChild(uviEl);
-                    // TODO: Color code UV index
-                    });
-                };
+                // Create humidity element and append
+                var humidityEl = document.createElement('p');
+                humidityEl.textContent = 'Humidity: ' + data.main.humidity + '%';
+                currentEl.appendChild(humidityEl);
+
+                // Create wind speed element and append
+                var windSpeedEl = document.createElement('p');
+                windSpeedEl.textContent = 'Wind Speed: ' + data.wind.speed + ' MPH';
+                currentEl.appendChild(windSpeedEl);
+
+                // Get lat/lon coordinates and use this to fetch UV index
+                var uviUrl = 'https://api.openweathermap.org/data/2.5/uvi?lat=' + data.coord.lat + '&lon=' + data.coord.lon + '&appid=dc2f3090dc723dd5dfe242a2abd2e604';
+                // Run fetch for this UVI URL and append 
+                fetch (uviUrl).then(function(response) {
+                    if (response.ok) {
+                        response.json().then(function(data) {
+                        var uviEl = document.createElement('p');
+                        uviEl.textContent = 'UV Index: ' + data.value;
+                        currentEl.appendChild(uviEl);
+                        // TODO: Color code UV index
+                        });
+                    };
+                });
+                // Create lat/lon coordinates and use URL this to fetch 7-day forecast
+                var oneCallUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + data.coord.lat + '&lon=' + data.coord.lon + '&units=imperial&exclude=minutely,hourly,alerts&appid=dc2f3090dc723dd5dfe242a2abd2e604';
+                fetch (oneCallUrl).then(function(response) {
+                    if (response.ok) {
+                        response.json().then(function(data) {
+                            
+                            for (var i = 1; i < 6; i++) {
+
+                                var cardEl = document.createElement('div');
+                                cardEl.setAttribute('class', 'card bg-primary text-light rounded p-2 m-2');
+                                forecastEl.appendChild(cardEl);
+
+                                var dateEl = document.createElement('h5');
+                                dateEl.textContent = moment().add(i, 'days').format('MM/DD/YYYY');
+                                cardEl.appendChild(dateEl);
+        
+                                // Create weather icon and append
+                                var icon2El = document.createElement('img');
+                                icon2El.setAttribute('src', 'https://openweathermap.org/img/wn/' + data.daily[i].weather[0].icon + '.png');
+                                cardEl.appendChild(icon2El);
+        
+                                // Create temperature element and append
+                                var temp2El = document.createElement('p');
+                                temp2El.textContent = 'Temp: ' + data.daily[i].temp.day + ' °F';
+                                cardEl.appendChild(temp2El);
+        
+                                // Create humidity element and append
+                                var humid2El = document.createElement('p');
+                                humid2El.textContent = 'Humidity: ' + data.daily[i].humidity + ' %';
+                                cardEl.appendChild(humid2El);
+                            };
+                        });
+                    };
+                });
             });
-            // Create lat/lon coordinates and use URL this to fetch 7-day forecast
-            var oneCallUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + data.coord.lat + '&lon=' + data.coord.lon + '&units=imperial&exclude=minutely,hourly,alerts&appid=dc2f3090dc723dd5dfe242a2abd2e604';
-            fetch (oneCallUrl).then(function(response) {
-                if (response.ok) {
-                    response.json().then(function(data) {
-                        
-                        for (var i = 1; i < 6; i++) {
+        } else {
+            currentEl.textContent = 'Invalid search. Please try again.'
+        };
+    });
+};
 
-                            var cardEl = document.createElement('div');
-                            cardEl.setAttribute('class', 'card bg-primary text-light rounded p-2 m-2');
-                            forecastEl.appendChild(cardEl);
 
-                            var dateEl = document.createElement('h5');
-                            dateEl.textContent = moment().add(i, 'days').format('MM/DD/YYYY');
-                            cardEl.appendChild(dateEl);
-    
-                            // Create weather icon and append
-                            var icon2El = document.createElement('img');
-                            icon2El.setAttribute('src', 'https://openweathermap.org/img/wn/' + data.daily[i].weather[0].icon + '.png');
-                            cardEl.appendChild(icon2El);
-    
-                            // Create temperature element and append
-                            var temp2El = document.createElement('p');
-                            temp2El.textContent = 'Temp: ' + data.daily[i].temp.day + ' °F';
-                            cardEl.appendChild(temp2El);
-    
-                            // Create humidity element and append
-                            var humid2El = document.createElement('p');
-                            humid2El.textContent = 'Humidity: ' + data.daily[i].humidity + ' %';
-                            cardEl.appendChild(humid2El);
-                        };
-                    });
-                };
-            });
-        });
-    } else {
-        currentEl.textContent = 'Invalid search. Please try again.'
-    };
+submitBtn.addEventListener('submit', function() {
+    var cityName = document.getElementById('city-input').value;
+    console.log(cityName);
+    getWeather(cityName);
 });
